89db7a29d5ad816f94e0d8c11f8393b522a0f0ed
cr139139 <40819960+cr139139@users.noreply.github.com>
2022-02-09 01:43:37 +0900

    Added REINFORCE algorithm
    -Added "reinforce.py" for REINFORCE algorithm (only training abstracts are included)
    -Changed "Actor" network (this needs to be discussed)
    -Changed "Aggregator" network

--- models.py
+++ models.py
@@ -1,20 +1,50 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 from googlenet import *
 
-class Actor(nn.Module):
-    def __init__(self):
-        super(Actor, self).__init__()
-        self.layer1 = nn.Linear(256, 35, bias=True)
 
-    def forward(self, a, p, num_samples):
-        input = torch.cat((a, p), dim=1)                #Check
+# fully connected discrete-action policy
+class Actor(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(Actor, self).__init__()
+        self.layer1 = nn.Linear(input_dim, output_dim, bias=True)
+
+    def _format(self, state):
+        x = state
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x, dtype=torch.float32)
+            x = x.unsqueeze(0)
+        return x
+
+    def forward(self, a, p):
+        input = self._format(torch.cat((a, p), dim=1))  #Check
         output = self.layer1(input)
         output = torch.clamp(output, 0, 1)              #clamp
         output = F.normalize(output, dim=0)             #L1-normalize
-        output = torch.multinomial(output, num_samples)
         return output
+
+    def full_pass(self, a, p):
+        logits = self.forward(a, p)
+        dist = torch.distributions.Categorical(logits=logits)
+        action = dist.sample()
+        logpa = dist.log_prob(action).unsqueeze(-1)
+        entropy = dist.entropy().unsqueeze(-1)
+        is_exploratory = action != np.argmax(logits.detach().numpy())
+        return action.item(), is_exploratory.item(), logpa, entropy
+
+    def select_action(self, a, p):
+        logits = self.forward(a, p)
+        dist = torch.distributions.Categorical(logits=logits)
+        action = dist.sample()
+        return action.item()
+
+    def select_greedy_action(self, a, p):
+        logits = self.forward(a, p)
+        return np.argmax(logits.detach().numpy())
+
+
 
 class Sensor(nn.Module):
     def __init__(self):
@@ -44,10 +74,11 @@ class Aggregator(nn.Module):
         self.relu = nn.ReLu()
         self.layer1 = nn.Linear(256, 256)
 
-    def forward(self, m, v):
+    def forward(self, a):
         # Can't understand what + means, so I just put concatenate
         # Plus we have to check whether append means stack or cat
-        input = torch.cat((m,v), dim=1)
+        # input = torch.cat((m,v), dim=1)
+        input = a
         output = self.relu(input)
         output = self.layer1(output)
         return output
